61ab447d0a2224b8ae1fee60e03e588fa71720d8
Skuzzzy <JamesDanielBaird@gmail.com>
2015-01-11 16:19:46 -0700

    Renaming and minor changes

--- getFiles.py
+++ getFiles.py
@@ -3,18 +3,19 @@ import os
 def getFolders():
 	return os.listdir('./extracted-with-links/')
 
-def getPathsFromFile(file):
+def getPathsFromDirectory(directory):
 	fullPaths = []
 
-	localFiles = os.listdir("./extracted-with-links/"+file)
+	localFiles = os.listdir("./extracted-with-links/"+directory)
 	for x in localFiles:
-		fullPaths.append("./extracted-with-links/"+file+"/"+x)
+		fullPaths.append("./extracted-with-links/"+directory+"/"+x)
 	return fullPaths 
 
 fullFileNames = []
 
 allDirectories = getFolders()
-for x in allDirectories:
-	fullFileNames = fullFileNames+getPathsFromFile(x)
-for x in fullFileNames:
-	print x
+for directory in allDirectories:
+	fullFileNames = fullFileNames+getPathsFromDirectory(directory)
+for fileName in fullFileNames:
+	print fileName 
+
